382f7ef76f1d67578ae1d94e6f50dff20c3ea2ff
johnn <johnn@192.168.1.176>
2017-09-04 03:55:08 +0100

    Added Junit Test

--- src/test/java/kpn/AppTest.java
+++ src/test/java/kpn/AppTest.java
@@ -1,10 +1,55 @@
 package kpn;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 public class AppTest {
 
     @Test
-    public void testApp() {
+    public void testCustomerOutrage() {
+
+        try {
+            File customerFile = new File("src/main/resources/customer_outages.json");
+        	
+            String customerOutrageAnswer = FileUtils.readFileToString(customerFile);
+            
+            Application.main(new String[]{"2017-07-20 08:11"});
+            
+            File customerOutputFile = new File("src/main/resources/runtime/customer_outages.json");
+            
+            String customerOutrageOutput = FileUtils.readFileToString(customerOutputFile);
+            
+            assertEquals(customerOutrageAnswer, customerOutrageOutput);
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void testBusinessOutrage() {
+        try {
+            File businessFile = new File("src/main/resources/business_outages.json");
+        	
+            String businessOutrageAnswer = FileUtils.readFileToString(businessFile);
+            
+            Application.main(new String[]{"2017-07-18 08:11"});
+            
+            File businessOutputFile = new File("src/main/resources/runtime/business_outages.json");
+            
+            String businessOutrageOuput = FileUtils.readFileToString(businessOutputFile);
+            
+            assertEquals(businessOutrageAnswer, businessOutrageOuput);
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
